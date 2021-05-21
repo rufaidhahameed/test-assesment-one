51ebf097c53df71ac12eae59a0ca0ee4bdf13a46
rufaidhahameed <rufaidhahameed@gmail.com>
2021-05-21 15:21:31 +0530

    initial starter for product microservice

---
+++ server.js
@@ -0,0 +1,136 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import MongoClient from "mongodb";
+import pkg from 'bson'
+
+/**
+ * mongodb url
+ */
+const uri = "mongodb://localhost:27017";
+const {ObjectId} = pkg
+
+var _db;
+
+/**
+ * function to connect to mongodb
+ */
+const MongoUtil = {
+    // create connection to mongodb
+  connectToServer: function (callback) {
+    MongoClient.connect(
+      uri,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      },
+      function (err, client) {
+        //   db name
+        _db = client.db("shop");
+        return callback(err);
+      }
+    );
+  },
+  // get connected db
+  getDb: function () {
+    return _db;
+  },
+};
+
+// port to start server
+const PORT = 3001;
+
+/**
+ * function to execute mongo connection and start server
+ */
+MongoUtil.connectToServer(function (err) {
+  const app = express();
+  app.use(express.json());
+// to enable bodyparser to get request.body from POST/PUT request
+  app.use(express.urlencoded({ extended: false }));
+  // to fix cross origin request 
+  app.use(cors({}));
+
+  // get db connection and db.collection
+  const db = mongoUtil.getDb();
+  const collection = db.collection('product')
+
+  /**
+ * CRUD - create read update delete methods
+ */
+
+/**
+ * execute api route for / to display welcome message
+ */
+app.use("/", (req, res) => { 
+    res.status(200).send("Server is Running for Shop Microservice");
+  });
+  
+  /**
+   * execute api route for fetching product data from db
+   */
+  app.get("/product", (req, res) => { 
+      collection.find().toArray(function (err, result) {
+        if (err) {
+        console.log(err)
+        res.status(500).send(err)
+        }
+        res.send(
+        result
+        )
+        });
+    });
+  
+  /**
+   * execute api route for inserting product data into db
+   */
+   app.post("/product", (req, res) => { 
+    collection.insertOne(req.body).then((result, err) => {
+        if (err) {
+        res.status(500).json(err)
+        }
+        res.send(
+        result
+        )
+        });
+    });
+  
+    /**
+   * execute api route for deleting product data from db
+   */
+  app.delete("/product", (req, res) => { 
+    collection.deleteOne({_id: ObjectId(req.query._id)}).then((result, err) => {
+        if (err) {
+        res.status(500).send(err)
+        }
+        res.send(
+        result
+        )
+        });
+    });
+  
+    /**
+   * execute api route for updating product data into  db
+   */
+  app.put("/product", (req, res) => { 
+    collection.updateOne({}, {$set: {' name': 1}}).then((result, err) => {
+        if (err) {
+        res.status(500).send(err)
+        }
+        res.send(
+        result
+        )
+        });
+    });
+  
+  
+  /**
+   * to start server using http module
+   */
+  http.createServer(app).listen(PORT, () => {
+    console.log("server started", PORT);
+  });
+
+});
+
+
