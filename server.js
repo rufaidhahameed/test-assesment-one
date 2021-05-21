bfa33ac0ecd251dfc2f0c6b0e7821baa61fa6c06
rufaidhahameed <rufaidhahameed@gmail.com>
2021-05-21 15:48:51 +0530

    more changes

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import MongoClient from "mongodb";
-import pkg from 'bson'
+import pkg from "bson";
+import cookieParser from 'cookie-parser'
+
 
 /**
  * mongodb url
  */
 const uri = "mongodb://localhost:27017";
-const {ObjectId} = pkg
+const { ObjectId } = pkg;
 
 var _db;
 
@@ -16,7 +18,7 @@ var _db;
  * function to connect to mongodb
  */
 const MongoUtil = {
-    // create connection to mongodb
+  // create connection to mongodb
   connectToServer: function (callback) {
     MongoClient.connect(
       uri,
@@ -46,91 +48,92 @@ const PORT = 3001;
 MongoUtil.connectToServer(function (err) {
   const app = express();
   app.use(express.json());
-// to enable bodyparser to get request.body from POST/PUT request
+  // to enable bodyparser to get request.body from POST/PUT request
   app.use(express.urlencoded({ extended: false }));
-  // to fix cross origin request 
+  app.use(cookieParser());
+  // to fix cross origin request
   app.use(cors({}));
 
   // get db connection and db.collection
-  const db = mongoUtil.getDb();
-  const collection = db.collection('product')
+  const db = MongoUtil.getDb();
+  const collection = db.collection("product");
 
   /**
- * CRUD - create read update delete methods
- */
+   * CRUD - create read update delete methods
+   */
 
-/**
- * execute api route for / to display welcome message
- */
-app.use("/", (req, res) => { 
-    res.status(200).send("Server is Running for Shop Microservice");
-  });
-  
   /**
    * execute api route for fetching product data from db
    */
-  app.get("/product", (req, res) => { 
-      collection.find().toArray(function (err, result) {
-        if (err) {
-        console.log(err)
-        res.status(500).send(err)
-        }
-        res.send(
-        result
-        )
-        });
+  app.get("/product", (req, res) => {
+    collection.find().toArray(function (err, result) {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      }
+      res.send(result);
     });
-  
+  });
+
   /**
    * execute api route for inserting product data into db
    */
-   app.post("/product", (req, res) => { 
+  app.post("/product", (req, res) => {
+      console.log("tag1", req.body)
     collection.insertOne(req.body).then((result, err) => {
-        if (err) {
-        res.status(500).json(err)
-        }
-        res.send(
-        result
-        )
-        });
+      if (err) {
+        res.status(500).json(err);
+      }
+      res.send(result);
     });
-  
-    /**
+  });
+
+  /**
    * execute api route for deleting product data from db
    */
-  app.delete("/product", (req, res) => { 
-    collection.deleteOne({_id: ObjectId(req.query._id)}).then((result, err) => {
+  app.delete("/product", (req, res) => {
+    collection
+      .deleteOne({ _id: ObjectId(req.query._id) })
+      .then((result, err) => {
         if (err) {
-        res.status(500).send(err)
+          res.status(500).send(err);
         }
-        res.send(
-        result
-        )
-        });
-    });
-  
-    /**
+        res.send(result);
+      });
+  });
+
+  /**
    * execute api route for updating product data into  db
    */
-  app.put("/product", (req, res) => { 
-    collection.updateOne({}, {$set: {' name': 1}}).then((result, err) => {
-        if (err) {
-        res.status(500).send(err)
-        }
-        res.send(
-        result
-        )
-        });
+  app.put("/product", (req, res) => {
+    collection.updateOne({}, { $set: { " name": 1 } }).then((result, err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.send(result);
     });
+  });
+
   
-  
+
+  /**
+   * execute api route for /server to display welcome message
+   */
+  app.use("/server", (req, res) => {
+    res.status(200).send("Server is Running for Shop Microservice");
+  });
+
+  /**
+   * execute api route for / to display welcome message
+   */
+   app.use("/*", (req, res) => {
+    res.status(404).send("API not found for Shop Microservice");
+  });
+
   /**
    * to start server using http module
    */
   http.createServer(app).listen(PORT, () => {
     console.log("server started", PORT);
   });
-
 });
-
-
